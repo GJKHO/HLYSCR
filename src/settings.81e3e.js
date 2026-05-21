0f241e7e1b3dacbe2f4da1bf8a974f5ea70b2ace
GJKHO <163989026+GJKHO@users.noreply.github.com>
2026-05-21 21:42:55 +0800

    Add files via upload

---
+++ src/settings.81e3e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","game3d"],collisionMatrix:[[true,true],[true,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/loading.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",remote:"cbfdd",resources:"902cb",main:"75c5c"}};
